49ceb30e52de2ae1e4c52db0b98921bb2566ab72
Benoit Paquin <you@example.combenoit@paquin.dk>
2022-12-29 14:48:57 +0100

    added requirements.txt

--- GetPokemon.py
+++ GetPokemon.py
@@ -1,3 +1,17 @@
-# This is a test
-2+2
-3+3
+# get list of pokemon
+import pokebase as pb
+chesto = pb.APIResource('berry', 'chesto')
+chesto.name
+'chesto'
+chesto.natural_gift_type.name
+'water'
+charmander = pb.pokemon('charmander')  # Quick lookup.
+charmander.height
+6
+# Now with sprites! (again!)
+s1 = pb.SpriteResource('pokemon', 17)
+s1.url
+s2 = pb.SpriteResource('pokemon', 1, other=True, official_artwork=True)
+s2.path
+s3 = pb.SpriteResource('pokemon', 3, female=True, back=True)
+s3.img_data
